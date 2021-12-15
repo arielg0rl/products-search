8fcda5d86e66dc11ec96b52688fb91dd15bc8eb7
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-12-15 18:04:32 +0200

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import './App.css';
+import './App.scss';
 import React, { useState, useEffect } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App">Hello goy
     </div>
   );
 }
