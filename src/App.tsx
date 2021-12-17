c62b0285855a1aed9afd15eac89cd5cb2eddf840
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-12-17 23:24:50 +0200

    filters done partially

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import './App.scss';
 import React, { useState, useEffect } from 'react';
+import './App.scss';
+import './components/FacetBlock/FacetBlock.scss';
+import './components/ProductList/ProductsList.scss';
+import './components/Filters/Filters.scss';
+import './components/Breadcrumbs/Breadcrumbs.scss';
+
+import Filters from './components/Filters/Filters';
+import ProductsList from './components/ProductList/ProductsList';
+import { getData } from './api';
 
 function App() {
+  const [ items, setItems ] = useState([]);
+  const [ facets, setFacets ] = useState([]);
+
+  useEffect(() => {
+    getData().then(loadedItems => {
+      setItems(loadedItems.items);
+      setFacets(loadedItems.facets);
+    });
+  }, [])
+
   return (
-    <div className="App">Hello goy
+    <div className="App">
+      <h1 className="App__heading">Search results</h1>
+      <Filters facets={facets}/>
+      <ProductsList items={items}/>
     </div>
   );
 }
